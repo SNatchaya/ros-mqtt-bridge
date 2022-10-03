137874f83c6f4d62bbd7a6e134f258ad31855daa
S. Natchaya <pv.natchaya@gmail.com>
2022-10-03 18:33:50 +0700

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -6,7 +6,7 @@ import sys
 from threading import Thread, Timer
 import paho.mqtt.client as mqtt
 from importlib import import_module
-from std_msgs.msg import String, Int32, Int32MultiArray, Float32MultiArray 
+from std_msgs.msg import String, Int32, Int32MultiArray, Float32MultiArray, Float64MultiArray
 
 def _finished_from_ros():
     print('{Ros->Mqtt} Transfer Finished : Received message from ROS and waiting for new message ...')
@@ -30,7 +30,7 @@ class RosToMqttBridge(Thread):
         self.qos        = 0
         self.client                 = mqtt.Client('Ros-to-Mqtt')
         self.client.connect(self.host, self.port)
-        self._ros_timer = Timer(1.0, _start_ros)
+        self._ros_timer = Timer(2.0, _start_ros)
         self._ros_timer.start()
 
         # List the topic names and type of these message
@@ -44,21 +44,19 @@ class RosToMqttBridge(Thread):
                 rospy.Subscriber(topic, msg_type, self._ros_callback, topic)
 
     def _ros_callback(self, message, topic):
-        self._ros_timer.cancel()
-        self._ros_timer = Timer(2.0, _finished_from_ros)
         # Change the message type of ROS, recommened convert to string type
         if type(message.data) == tuple:
             ros_message = str(message.data)
         else:
             ros_message = message.data
-        
         self.client.publish(topic, ros_message, self.qos)
+        self._ros_timer.cancel()
+        self._ros_timer = Timer(2.0, _finished_from_ros)
         self._ros_timer.start()
 
     def run(self):
         self.client.loop_start()
         rospy.spin()
-
 
 class MqttToRosBridge(Thread):
     def __init__(self):
@@ -88,7 +86,6 @@ class MqttToRosBridge(Thread):
         print(f'MQTT topic:')
         print(f'            /exvis/sub/#')
 
-
 def main():
     try:
         print('\n--------------------------------- Start the ROS-MQTT bridge ---------------------------------\n\n')
@@ -102,7 +99,7 @@ def main():
     except KeyboardInterrupt:
         print('Stop to run the ROS-MQTT bridge ...')
         mqtt.Client.disconnect()
-        sys.exit(1)
+        sys.exit()
 
 if __name__ == '__main__':
     main()
